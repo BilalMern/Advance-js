9ba23eff150ee1a7fe225cd85cfa0272c247b240
BilalMern <bilal.mern@gmail.com>
2023-09-12 00:55:04 +0500

    array values using spread operator

--- Spread Operator/app.js
+++ Spread Operator/app.js
@@ -1,9 +1,36 @@
-function foo(name,...args){
-    let sum=0;
-    for(let i in args){
-        sum+=args[i]
-    }
-    console.log(`hello ${name} Your sum is ${sum}`)
-}
-let arr=[2,4,3]
-foo("billa",...arr)
+// function foo(name,...args){
+//     let sum=0;
+//     for(let i in args){
+//         sum+=args[i]
+//     }
+//     console.log(`hello ${name} Your sum is ${sum}`)
+// }
+// let arr=[2,4,3]
+// foo("billa",...arr)
+
+// function foo(name,...args){
+//     let sum=0;
+//     for(let i in args){
+//         sum+=args[i]
+//     }
+//     console.log(`${name} Your marks are ${sum}`)
+// }
+// foo("billa",2,3,4)
+
+
+
+// function foo1(name,...args1){
+//     let sum1=0;
+//     for (let i in args1){
+//         sum1+=args1[i]
+//     }
+// console.log(`${name} Your marks are ${sum1}`)
+// }
+// let arr1=[3,5,4]
+// foo1("billa",...arr1)
+
+
+
+
+let arr=[2,3,4,5,6]
+console.log(...arr)
