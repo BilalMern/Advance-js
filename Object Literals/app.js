beabd6cf6d59164148f3fc85cfcdca37f680fa9a
BilalMern <bilal.mern@gmail.com>
2023-09-12 18:29:07 +0500

    new object methods

---
+++ Object Literals/app.js
@@ -0,0 +1,19 @@
+// let obj={
+//     name: "bilal",
+//     roll: 123
+// }
+// console.log(obj)
+
+
+// let name="bilal"
+// let obj={
+//     name
+// }
+// console.log(obj)
+
+
+// let n= "name"
+// let obj={
+//     [n + " " + "is"]: "bilal"
+// }
+// console.log(obj)
