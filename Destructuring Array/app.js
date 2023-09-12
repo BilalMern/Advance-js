0a476d4f0c10c9261c11bb8c2cb415037c009085
BilalMern <bilal.mern@gmail.com>
2023-09-13 00:46:22 +0500

    Destructuring Object

--- Destructuring Array/app.js
+++ Destructuring Array/app.js
@@ -3,6 +3,12 @@
 // let num=arr[1];
 // console.log(name)
 
-let arr=["bilal",2];
-let [name,age]=arr;
-console.log(name)
+// let arr=["bilal",2];
+// let [name,age]=arr;
+// console.log(name)
+
+// function foo(){
+//     return ["bilal",26,"karachi"]
+// }
+// let[name,age,city]=foo()
+// console.log(foo())
