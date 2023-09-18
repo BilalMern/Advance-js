5980b0c0c8e4915989ca7baeeb66646a9f3b1f8c
BilalMern <bilal.mern@gmail.com>
2023-09-18 20:24:58 +0500

    reject reslove

--- Js Promises/app.js
+++ Js Promises/app.js
@@ -1,3 +1,19 @@
-const prom= new Promise (function(resolve,reject){
-    
-})
+let prom= new Promise(function(resolve,reject){
+  var age=+prompt("Enter your age")
+  if(age>=18){
+resolve()
+
+}  
+  else{
+    reject();
+   
+  }
+});
+let onresolve=()=>{
+    console.log("you are eligiblel")
+}
+let onrejection=()=>{
+    console.log("not eligible")
+}
+prom.then(onresolve)
+prom.catch(onrejection)
