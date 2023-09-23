77ba34dddae3c66f37e787c4d123478d9319669f
BilalMern <bilal.mern@gmail.com>
2023-09-23 23:00:35 +0500

    callback functios

--- Asynchronous Js/app.js
+++ Asynchronous Js/app.js
@@ -1,5 +1,16 @@
-console.log("hello there")
-console.log("hello there")
-console.lo("hello there")
-console.log("hello there")
-console.log("hello there")
+// console.log("hello there")
+// console.log("hello there")
+// console.lo("hello there")
+// console.log("hello there")
+// console.log("hello there")
+
+
+function one(){
+console.log("Step one cmplt plz call step 2")
+
+}
+
+function two(){
+console.log("Step two")
+}
+one(two())
