e424fe47ee40d6183607b71fb8475aa155df3ac6
BilalMern <bilal.mern@gmail.com>
2023-09-24 22:08:07 +0500

    realtion btw functions and objects with asynchronous

--- Asynchronous Js/app.js
+++ Asynchronous Js/app.js
@@ -39,7 +39,7 @@
 //     setTimeout(function(){
 //         calling_2()
 //         console.log("hello foo1")
-//     },3000)
+//     },2000)
 // }
 // function foo2(){
 //     setTimeout(function(){
@@ -49,14 +49,56 @@
 // foo1(foo2);
 
 
-function foo1(){
+// function foo1(){
+//     setTimeout(function(){
+//         console.log("hello foo1")
+//     },3000)
+// }
+// function foo2(){
+//     setTimeout(function(){
+//         console.log("hello foo2")
+//     },1000)
+// }
+// foo1(foo2());
+
+
+
+// function foo1(calling_2){
+//     console.log("hello 1")
+//     calling_2()
+// }
+// function foo2(){
+//     console.log("hello 2")
+// }
+// foo1(foo2)
+
+
+// function order(calling_production){
+//     console.log("order placed, please call productionn")
+// calling_production()
+// }
+// function productionn(){
+//     console.log("order is recieved. Starting Production")
+
+// }
+// order(productionn)
+
+
+let stokes={
+    Fruits: ["strawberry","grapes","banana","apple"],
+    Liquid: ["water","ice"],
+    Holder: ["cone","cup","stick"],
+    Toppings:["chocolates","peanuts"]
+};
+
+
+function order(fruit_name,calling_production){
     setTimeout(function(){
-        console.log("hello foo1")
-    },3000)
+        console.log(`${stokes.Fruits[fruit_name]}`)
+    },2000)
+calling_production()
 }
-function foo2(){
-    setTimeout(function(){
-        console.log("hello foo2")
-    },1000)
+function production(){
+
 }
-foo1(foo2());
+order(0,production)
