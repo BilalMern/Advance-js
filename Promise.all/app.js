4c364b3d0b45fa77e4965374d6a4511338d47e63
BilalMern <bilal.mern@gmail.com>
2023-09-29 01:29:21 +0500

    last exercise of promise.all

--- Promise.all/app.js
+++ Promise.all/app.js
@@ -1,16 +1,39 @@
-let p1= new Promise((resolve,reject)=>{
-    console.log("promise one has resolved")
-    resolve(10)
+// let p1= new Promise((resolve,reject)=>{
+//     console.log("promise one has resolved")
+//     resolve(10)
+// })
+
+// let p2= new Promise((resolve,reject)=>{
+//     console.log("promise two has resolved")
+//     resolve(20)
+// })
+// let p3=new Promise((resolve,reject)=>{
+//     console.log("promise has rejected")
+//     reject("We are sorry");
+// })
+// Promise.all([p1,p2,p3]).then((result)=>{
+//     console.log(`result: ${result}`)
+// }).catch((error)=>{
+//     console.log(error)
+// })
+
+
+let promiseCall = (function (data, message) {
+    return function (resolve, reject) {
+setTimeout(function(){
+    console.log(`the ${message} promise has resolved`)
+    resolve(data)
+    reject("we are sorry")
+},data*100)
+    }
 })
 
-let p2= new Promise((resolve,reject)=>{
-    console.log("promise two has resolved")
-    resolve(20)
-})
-Promise.all([p1,p2]).then((result)=>{
-    console.log(`result: ${result}`)
+let prome1= new Promise(promiseCall(10,"first"))
+let prome2= new Promise(promiseCall(20,"second"))
+let prome3= new Promise(promiseCall(30,"third"))
+
+Promise.all([prome1,prome2,prome3]).then((res)=>{
+console.log(`Result:${res}`)
 }).catch((error)=>{
     console.log(error)
 })
-
-
