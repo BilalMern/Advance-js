5c274388f4b3acfcf41a727e39f5ae3c102ecd22
BilalMern <bilal.mern@gmail.com>
2023-10-02 20:46:48 +0500

    dom in ajax

--- Ajax/app.js
+++ Ajax/app.js
@@ -1,10 +1,22 @@
-function loadData(){
-    let xhttp= new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status ==200){
-            console.log(this.responseText)
-        }
+// function loadData(){
+//     let xhttp= new XMLHttpRequest();
+//     xhttp.onreadystatechange = function(){
+//         if(this.readyState == 4 && this.status ==200){
+//             console.log(this.responseText)
+//         }
+//     }
+//     xhttp.open('GET', "/file/data.txt", true);
+//     xhttp.send()
+// }
+
+let loadData = ()=>{
+let xhttp= new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status==200) {
+        console.log(this.responseText)
     }
-    xhttp.open('GET', "/file/data.txt", true);
-    xhttp.send()
+}
+
+xhttp.open('GET', "file/data.txt", true)
+xhttp.send()
 }
