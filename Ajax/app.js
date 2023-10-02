ca4e5c04bb28543de179800bd16f9cd221035439
BilalMern <bilal.mern@gmail.com>
2023-10-02 20:50:42 +0500

    error in ajax

--- Ajax/app.js
+++ Ajax/app.js
@@ -1,7 +1,7 @@
-// function loadData(){
+// let loadData=()=>{
 //     let xhttp= new XMLHttpRequest();
 //     xhttp.onreadystatechange = function(){
-//         if(this.readyState == 4 && this.status ==200){
+        // if(this.readyState == 4 && this.status ==200){
 //             console.log(this.responseText)
 //         }
 //     }
@@ -9,14 +9,20 @@
 //     xhttp.send()
 // }
 
-let loadData = ()=>{
-let xhttp= new XMLHttpRequest();
-xhttp.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status==200) {
-        console.log(this.responseText)
-    }
-}
 
-xhttp.open('GET', "file/data.txt", true)
-xhttp.send()
+
+
+let loadData = ()=> {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState ==4 && this.status == 200){
+            document.getElementById("demo").innerHTML = this.responseText
+        }else if(this.readyState ==4 && this.status == 404){
+            document.getElementById("demo").innerHTML = "your data not found"
+
+        }
+    }
+    xhttp.open('GET',"/file/data.txt",true);
+    xhttp.send();
+    
 }
