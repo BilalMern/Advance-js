f32b1a0affd85c5c848f9d667e6e2ee9ad6a8369
BilalMern <bilal.mern@gmail.com>
2023-10-11 13:08:22 +0500

    Fetching nested object in document

--- Fetch/app.js
+++ Fetch/app.js
@@ -11,28 +11,44 @@
 //         document.write(`${data[x].name}-${data[x].email} <br> `)
 //     }
 // })
-var obj = {
+// var obj = {
 
-    obj1: {
-        name: "bilal",
-        age: 18,
-        city: "karachi"
-    },
-    obj2: {
-        name: "qaiser",
-        age: 17,
-        city: "islamabad"
-    },
+//     obj1: {
+//         name: "bilal",
+//         age: 18,
+//         city: "karachi"
+//     },
+//     obj2: {
+//         name: "qaiser",
+//         age: 17,
+//         city: "islamabad"
+//     },
 
-    obj3: {
-        name: "talha",
-        age: 20,
-        city: "lahore"
-    }
+//     obj3: {
+//         name: "talha",
+//         age: 20,
+//         city: "lahore"
+//     }
+
+// }
+// for(let i in obj){
+//     console.log(obj[i])
+// }
+
+let obj = {
+
+obj1:{
+    name:"bilal",
+    age: 18
+},
+obj2:{
+    name: "qaiser",
+    age:20
+}
 
 }
 for(let i in obj){
-    console.log(obj[i])
+    console.log(obj[i].name)
 }
 
 
