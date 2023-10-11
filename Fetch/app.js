dfa781b0ed4b8db18533b461152ee78198ebf96e
BilalMern <bilal.mern@gmail.com>
2023-10-11 13:02:33 +0500

    fetching nested objects

--- Fetch/app.js
+++ Fetch/app.js
@@ -4,6 +4,35 @@
 // })
 // fetch("context/readme.txt") .then(function(result){return result.text()}) 
 // // OR
-fetch ("https://jsonplaceholder.typicode.com/users") .then( result => result.json())
-.then((data)=>{console.log(data)})
+// fetch ("https://jsonplaceholder.typicode.com/users") .then( result => result.json())
+// .then((data)=>{
+//     console.log(data)
+//     for(let x in data){
+//         document.write(`${data[x].name}-${data[x].email} <br> `)
+//     }
+// })
+var obj = {
+
+    obj1: {
+        name: "bilal",
+        age: 18,
+        city: "karachi"
+    },
+    obj2: {
+        name: "qaiser",
+        age: 17,
+        city: "islamabad"
+    },
+
+    obj3: {
+        name: "talha",
+        age: 20,
+        city: "lahore"
+    }
+
+}
+for(let i in obj){
+    console.log(obj[i])
+}
+
 
