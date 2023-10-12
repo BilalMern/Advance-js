8a45e3cc79c1a7b5e95e1579ee5d07837cef0932
BilalMern <bilal.mern@gmail.com>
2023-10-13 01:03:41 +0500

    async await with fetch

--- Assync Await/app.js
+++ Assync Await/app.js
@@ -7,12 +7,25 @@
 
 
 
-async function test() {
-    console.log(2)
-    console.log(3)
-    console.log(4)
-    console.log(5)
+// async function test() {
+//     console.log(2)
+//     console.log(3)
+//     await console.log(4)
+//     console.log(5)
+// }
+// console.log(1)
+// test()
+// console.log(6)
+
+async function test(){
+    console.log("2: Message")
+    const response = await fetch("json/student_data.json");
+    // const students = await response.json();
+    console.log("3: Message")
+    return await response.json()
 }
-console.log(1)
-test()
-console.log(6)
+console.log("1: Message");
+test();
+console.log("4: Message");
+console.log(test())
+
