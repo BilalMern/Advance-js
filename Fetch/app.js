36bf086ce879c16af5ed905d7f2e7b34f754a56d
BilalMern <bilal.mern@gmail.com>
2023-10-12 11:44:26 +0500

    Assync Await

--- Fetch/app.js
+++ Fetch/app.js
@@ -87,23 +87,19 @@
 //   .then((json) => console.log(json));
 
 
-fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    body: JSON.stringify({
-        title: 'foo',
-        body: 'bar',
-        userId: 1,
-    }),
-    headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-    },
-})
-    .then((response) => response.json())
-    .then((json) => {
-        console.log(json)
-        
-            document.write(`${json.title}`)
-            
-        
-
-    });
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//         title: 'foo',
+//         body: 'bar',
+//         userId: 1,
+//     }),
+//     headers: {
+//         'Content-type': 'application/json; charset=UTF-8',
+//     },
+// })
+//     .then((response) => response.json())
+//     .then((json) => {
+//         console.log(json)
+//         document.write(`${json.userId}`)
+//     });
