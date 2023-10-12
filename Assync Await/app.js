92556d11672cf203cf1f16cd134ea0c11c9ea04d
BilalMern <bilal.mern@gmail.com>
2023-10-12 11:57:44 +0500

    testing senarios

---
+++ Assync Await/app.js
@@ -0,0 +1,18 @@
+// async function foo(){
+// return "Hello";
+// }
+// foo().then((result)=>{
+//     console.log(result)
+// })
+
+
+
+async function test() {
+    console.log(2)
+    console.log(3)
+    console.log(4)
+    console.log(5)
+}
+console.log(1)
+test()
+console.log(6)
