0d92997c6c1eec6b4bde09124ac5056bb7d84e81
BilalMern <bilal.mern@gmail.com>
2024-09-06 00:24:38 -0700

    Array optional chaining

--- Optional Chaining/app.js
+++ Optional Chaining/app.js
@@ -15,3 +15,19 @@ const user = {
     }
 }
 console.log(user.address.city)
+//* Above code give us a output Kararchi.
+
+//* But if we log the property which is not present in object then it will throw us a error which stops our execution.//
+//console.log(user.address.roadNumber.houseNumber) //To avoid the error we use optional chaining by which it wont throw an error it just send us undefined rather then error.
+
+console.log(user.address.roadNumber ?. houseNumber) // This means if roadNumber exists and its value is not null or undefined then and then only give the houseNumber value else not.
+
+// Optional chaining can also be used to access elements of an array in javaScript. It works in a simila way to accessing properties of an object.
+
+const user2=[
+    {name:"bilal", age: 25},
+    {name: "shazil", age: 20},
+    {name: "ayan", age: 30}
+]
+console.log(user2[3] ?. name)
+// it will give us undefined as we dont have 3 index.
